ed8a185f5619f4829644a11a3a1ab478c731ee90
kevinnmccabe-PC <kevin.n.mccabe@gmail.com>
2026-09-19 23:17:08 -0400

    Test rates mark-direction contract

--- tests/test_trader_room_ondemand.py
+++ tests/test_trader_room_ondemand.py
@@ -265,6 +265,27 @@ class SchemaAndBoundaryTests(unittest.TestCase):
         with self.assertRaises(DataBoundaryError):
             validate_contribution(contribution, packet=packet, expected_agent="perma-bull")
 
+    def test_rates_open_requires_unambiguous_mark_direction(self):
+        packet = _packet()
+        contribution = DryRunRunner().run_advocate("rate-hawk", packet, BudgetLedger())
+        contribution["paper_actions"] = [{
+            "action": "OPEN",
+            "instrument": "SOFR_2027-09",
+            "asset_class": "rates",
+            "side": "long",
+            "notional_usd": 10_000_000,
+            "expected_mark_direction": "higher",
+        }]
+        with self.assertRaises(SchemaError):
+            validate_contribution(contribution, packet=packet, expected_agent="rate-hawk")
+
+        contribution["paper_actions"][0]["side"] = "short"
+        validate_contribution(contribution, packet=packet, expected_agent="rate-hawk")
+
+        contribution["paper_actions"][0].pop("expected_mark_direction")
+        with self.assertRaises(SchemaError):
+            validate_contribution(contribution, packet=packet, expected_agent="rate-hawk")
+
     def test_contribution_requires_explicit_book_decision(self):
         packet = _packet()
         contribution = DryRunRunner().run_advocate("perma-bull", packet, BudgetLedger())
